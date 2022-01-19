3d994d97e8fe0452406b6ff276ff905531be75d7
Hasan Basri Ayaşlı <ayaslihasanbasri@gmail.com>
2022-01-19 21:03:02 +0300

    Network Error

--- src/component/error/error.jsx
+++ src/component/error/error.jsx
@@ -2,7 +2,8 @@ import './error.scss'
 const Error = ({ error }) => {
   return (
     <div className="error">
-      <h1>{error || 'Error !'}</h1>
+      <h1>{error}</h1>
+      <h1>{' You should : npm run db'}</h1>
     </div>
   );
 };
